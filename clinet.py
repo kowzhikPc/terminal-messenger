a530d4f0001ade889af5fffa71451b62b540db70
kowzhikPc <kowzhik.gs.dev@gmail.com>
2026-02-28 00:06:13 +0530

    not ready 2

--- clinet.py
+++ clinet.py
@@ -5,10 +5,17 @@ client =socket.socket()
 client.connect(("localhost",9999))
 i =1 
 while i != 0:
-    a =(client.recv(1024).decode())
-    if a != "q":
-        print(a)
-    else:
-        client.close()
-        print("connetion closed!")
-        i = 0
+    a = client.recv(1024).decode()
+    if a == "y":
+        x = input("Enter message to send(q for quit) - ")
+        if x == "q":
+            print("connection closed")
+            client.send("q".encode())
+            client.close()
+            i = 0
+        else:
+            client.send(x.encode())
+    elif a == "n":
+        a1 = client.recv(1024).decode()
+        print(a1)
+sys.exit(0)
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+import sys
 server = socket.socket()
 
 server.bind(("localhost",9999))
@@ -9,12 +9,27 @@ server.listen()
 conn,addr = server.accept()
 print(f"connected to{addr}")
 i = 0
+
 while i == 0:
-    a = input("Type something that you want to send to client(q for quit)-")
-    if a == "q":
-        conn.send("q".encode())
+    r = input(" Receive mode(y/n) - ")
+    if r == "y":
+        i+=1
+    else: 
+        conn.send("n".encode())
+        a = input("Type something that you want to send to client(q for quit)-")
+        if a == "q":
+            conn.send("q".encode())
+            conn.close()
+            print("connection closed!")
+            i +=1
+        else:
+            conn.send(a.encode())
+while True:
+    conn.send("y".encode())
+    a1 = conn.recv(1024).decode()
+    if a1 == "q":
         conn.close()
         print("connection closed!")
-        i +=1
+        sys.exit(0)
     else:
-        conn.send(a.encode())
+        print(a1)
